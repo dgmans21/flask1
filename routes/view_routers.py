d4ea4e7a222645c96ec8becc4c5b78b6a2068598
dgmans21 <dgdtkjk21@gmail.com>
2026-06-15 14:16:51 +0900

    Revert "1st commit"
    This reverts commit 5db6e83129026576624df6d5b09016971c8453ec.

--- routes/view_routers.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template    
-
-view_bp = Blueprint('view_bp', __name__, url_prefix='/')
-
-@view_bp.route('/')
-def home():
-    return render_template('index.html')
