3328dfd1a485d459ba33323a6d423ea00533f738
dgmans21 <dgdtkjk21@gmail.com>
2026-06-19 13:53:17 +0900

    ui fix
    ui수정 cors 주소처리 기반마련

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.json.ensure_ascii = False
 from routes.view_routers import view_bp
 from routes.test_router import test_bp  
